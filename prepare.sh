6c93b9a3c821484584eaa68242f16912043d8ecf
Samsara <68888258+JiNanPiWang@users.noreply.github.com>
2024-11-21 05:25:44 +0000

    update data

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 sudo apt-get update
 sudo apt-get install -y libgl1-mesa-glx
 pip3 install pdf2zh
 
-echo "remember to set: export OPENAI_BASE_URL=https://api.openai.com/v1"
-echo "remember to set: export export OPENAI_API_KEY=xxx"
+echo "remember to set: export OPENAI_BASE_URL=https://api.mixrai.com/v1"
+echo "remember to set: export OPENAI_API_KEY=xxx"
+echo "run with: pdf2zh files/GPGPUArch.pdf -s openai:gpt-4o-2024-05-13"
