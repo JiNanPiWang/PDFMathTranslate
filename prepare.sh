d6add022d1038d629b10e88a85ab4ad63e346814
Samsara <68888258+JiNanPiWang@users.noreply.github.com>
2024-11-21 02:54:56 +0000

    add pre

---
+++ prepare.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install -y libgl1-mesa-glx
+pip3 install pdf2zh
+
+echo "remember to set: export OPENAI_BASE_URL=https://api.openai.com/v1"
+echo "remember to set: export export OPENAI_API_KEY=xxx"
